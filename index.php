14441652c9ff4ed9550b626f8645f2b527a3c651
Francesco-Rianna <maradonino98@hotmail.it>
2024-05-15 18:19:41 +0200

    feat: print card in page

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $bosch = new Product('Bosch', 'cibo per cani', 20 ,'cani','https://shop-cdn-m.me
 // var_dump($bosch);
 $dogBed = new DogProduct('Dog Bed', 'spaziosa e comooda', 50, 'cani', 'Cuccia in legno','https://arcaplanet.vtexassets.com/arquivos/ids/216811/https---www.arcaplanet.it-media-catalog-product--b-a-baita-ferc14_1.jpg?v=637454582543530000');
 // var_dump($dogBed);
-$omegaSpray = new CatProduct('Omega','spray per il pelo del gatto', 19, 'gatti','pelo lungo','https://shop-cdn-m.mediazs.com/bilder/0/400/435002_pla_felisept_fellpflege_faltschachtel_flasche_hs_01_0.jpg');
+$omegaSpray = new CatProduct('Omega','spray per il pelo del gatto', 19, 'gatti','https://shop-cdn-m.mediazs.com/bilder/0/400/435002_pla_felisept_fellpflege_faltschachtel_flasche_hs_01_0.jpg','pelo lungo');
 // var_dump($omegaSpray);
 
 
@@ -20,7 +20,11 @@ $omegaSpray = new CatProduct('Omega','spray per il pelo del gatto', 19, 'gatti',
 // echo $bosch->getProduct() . '<br/>'; 
 // echo $dogBed->getDogProduct() . '<br/>';
 // echo $omegaSpray->getCatProduct() . '<br/>';
-
+$products = [
+    $bosch,
+    $dogBed,
+    $omegaSpray
+];
 
 ?>
 
@@ -35,12 +39,28 @@ $omegaSpray = new CatProduct('Omega','spray per il pelo del gatto', 19, 'gatti',
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <!-- <?php echo $bosch->getProduct(); ?> -->
-    </div>
-
-    <div>
-        <!-- <?php echo $dogBed->kennels ; ?> -->
+    <div class="container">
+        <h1 class='text-center'>I nostri prodotti</h1>
+        <div class="row">
+            <?php foreach ($products as $product) { ?>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <img src="<?php echo $product->image; ?>" class="card-img-top" alt="<?php echo $product->name; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $product->name; ?></h5>
+                            <p class="card-text"><?php echo $product->description; ?></p>
+                            <p class="card-text">Prezzo: <?php echo $product->price; ?>€</p>
+                            <p class="card-text">Categoria: <?php echo $product->category; ?></p>
+                            <?php if ($product instanceof DogProduct) { ?>
+                                <p class="card-text">Tipo Cuccia: <?php echo $product->kennels; ?></p>
+                            <?php } elseif ($product instanceof CatProduct) { ?>
+                                <p class="card-text">Tipo di pelo: <?php echo $product->furType; ?></p>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </div>
 </body>
 </html>
